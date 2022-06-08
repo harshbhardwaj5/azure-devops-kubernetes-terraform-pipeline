95cbf3cb7b80aa7600fd365b1712dd46965ff018
harshbhardwaj5 <47530172+harshbhardwaj5@users.noreply.github.com>
2022-06-09 00:51:59 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -8,6 +8,7 @@ terraform {
       version = "~>2.65"
     }
   }
+}
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
@@ -43,11 +44,4 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
-  }
-}
+
